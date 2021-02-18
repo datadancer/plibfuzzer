bc466a4623e1aeef6384c7740bfcf7c89a85db04
EricLiuCook <EricLiuCook@gmail.com>
2021-02-18 12:45:56 +0800

    pop one

--- FuzzerShare.cpp
+++ FuzzerShare.cpp
@@ -165,7 +165,7 @@ void PopInputInfo(Vector<InputInfo*> &IIV) {
     void * LogStart;
     struct InputInfoLog * logs; 
 
-    for(int i=0; i<TOTAL; i++){
+    /*for(int i=0; i<TOTAL; i++){
 	if (i == ID) continue;
     //int i =ID+1;
 	//if (ID==TOTAL-1)
@@ -184,7 +184,22 @@ void PopInputInfo(Vector<InputInfo*> &IIV) {
 		NumberOfPopedLogs++;
 	}
 	TAILS[i] = * head; //Update current tail
-    }
+    }*/
+     int i =ID+1;
+     if (ID==TOTAL-1)i=0;
+     void * CurrentNeighborLog = NeighborLog[i];
+     if(CurrentNeighborLog == NULL)  {
+	 Printf("Error: Neighbor Log is null, skip popping.\n");
+     }
+     head = (long *)CurrentNeighborLog;
+     LogStart = (char *)CurrentNeighborLog + sizeof(HEAD);
+     logs = (struct InputInfoLog *)LogStart;
+     for(long j=TAILS[i]; j < *head; j++){
+	 //Printf("Popping %d/%d from %d\n", j, *head, i);
+	 IIV.push_back(PopOneInputInfo(logs + (j % NUM_LOGS)));
+	 NumberOfPopedLogs++;
+     }
+     TAILS[i] = * head; //Update current tail
 }
 
 void IncreaseNumberOfIntrestingPopedLogs(){
